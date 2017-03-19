cc38fdb4383ad0e1f772688ebfe0dbb4a278ba1f
kanonenfutter <van.kanonenfutter@web.de>
2017-03-19 22:23:20 +0100

    code removed

--- routes/index.js
+++ routes/index.js
@@ -8,15 +8,6 @@ const host = 'https://api.go-tellm.com';
 var headers = {
 			'User-Agent': 'Jodel/4.37.5 Dalvik/2.1.0 (Linux; U; Android 7.1; P6000 Build/NDE63P)',
 			'Authorization': 'INSERT YOUR KEY',
-			'X-Client-Type': 'android_4.37.5',
-			'X-Api-Version': '0.2',
-			'Connection': 'Keep-Alive',
-			'Accept-Encoding': 'gzip'
-		};
-
-var header2 = {
-			'User-Agent': 'Jodel/4.37.5 Dalvik/2.1.0 (Linux; U; Android 7.1; P6000 Build/NDE63P)',
-			'Authorization': 'INSERT YOUR KEY',
 		};
 
 /* GET home page. */
@@ -29,12 +20,37 @@ router.get('/recent', function(req, res, next) {
 	var recentJodels = '';
 	var options = {
 		uri: host + '/api/v3/posts/location/combo',
-		headers: header2,
+		headers: headers,
 		json: true,
 		method: 'GET',
 		qs: {
 			lat: 50.94129,
 			lng: 6.95817
+		}
+
+	};
+	request(options)
+		.then(function (data){
+			console.log(data.recent);
+			res.json(data);
+		})
+		.catch(function (err) {
+			console.log(err);
+			res.end();
+		});
+
+});
+
+router.get('/recent/:latitude/:longitude', function(req, res, next) {
+	var recentJodels = '';
+	var options = {
+		uri: host + '/api/v3/posts/location/combo',
+		headers: headers,
+		json: true,
+		method: 'GET',
+		qs: {
+			lat: latitude,
+			lng: longitude
 		}
 
 	};
@@ -49,28 +65,5 @@ router.get('/recent', function(req, res, next) {
 		});
 
 });
-
-
-function getRecent(){
-	var options = {
-		uri: host + '/api/v3/posts/location/combo',
-		headers: header2,
-		json: true,
-		method: 'GET',
-		qs: {
-			lat: 50.8198311,
-			lng: 6.8913095
-		}
-
-	};
-	request(options)
-		.then(function (res){
-			console.log(JSON.stringify(res));
-		})
-		.catch(function (err) {
-		// body...
-		});
-
-}
 
 module.exports = router;
