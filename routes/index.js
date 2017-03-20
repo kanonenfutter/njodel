e323f1a03f1707d7230bc4e358328c0418792aa7
kanonenfutter <van.kanonenfutter@web.de>
2017-03-20 14:31:05 +0100

    formatting, key

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ const host = 'https://api.go-tellm.com';
 
 var headers = {
 			'User-Agent': 'Jodel/4.37.5 Dalvik/2.1.0 (Linux; U; Android 7.1; P6000 Build/NDE63P)',
-			'Authorization': 'INSERT YOUR KEY',
+			'Authorization': 'Bearer 04272352-726d639e-88cfc995-38db-4cf2-a74b-1272ad4628d2',
 		};
 
 /* GET home page. */
@@ -24,8 +24,9 @@ router.get('/recent', function(req, res, next) {
 		json: true,
 		method: 'GET',
 		qs: {
-			lat: 50.94129,
-			lng: 6.95817
+			lat: 50.819923400878906,
+			lng: 6.8911275863,
+			home: true
 		}
 
 	};
@@ -65,5 +66,30 @@ router.get('/recent/:latitude/:longitude', function(req, res, next) {
 		});
 
 });
+
+router.get('/posts/:id', function (req, res, next) {
+	// body...
+	var data = '';
+	var options = {
+		uri: host + '/api/v3/posts/location/combo',
+		headers: headers,
+		json: true,
+		method: 'GET',
+		qs: {
+			lat: latitude,
+			lng: longitude
+		}
+
+	};
+	request(options)
+		.then(function (data){
+			console.log(data);
+			res.json(data);
+		})
+		.catch(function (err) {
+			console.log(err);
+			res.end();
+		});
+})
 
 module.exports = router;
